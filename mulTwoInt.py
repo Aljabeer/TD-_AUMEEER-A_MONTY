adda88f2964a9d998838bec7cdaf025160b4ef23
Oliverm12 <montyoliver12@hotmail.com>
2021-03-16 15:08:14 +0400

    affichage d'un mesage erreur lors d'une entree invalide

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -2,9 +2,26 @@
 
 import sys
 
-print(sys.argv)
+def mul (x,y):
+	return x*y
 
-x = int( sys.argv[1] )
-y = int( sys.argv[2] )
+if __name__=="__main__":
+        if ( len(sys.argv) >  3):
+                print("Entrez que 2 variable")
 
-print(x*y)
+        elif ( len(sys.argv) == 1 ):
+                x = input("Premier valeur: ")
+                y = input("Deuxieme valeur: ")
+                x = int(x)
+                y = int(y)
+                print(mul(x,y))
+        elif (len(sys.argv) == 2 ):
+                print("Entrez encore une valeur")
+                y = input("Le deuxieme valeur: ")
+                x = int( sys.argv[1] )
+                y = int(y)
+                print(mul(x,y))
+        else:
+                x = int( sys.argv[1] )
+                y = int( sys.argv[2] )
+                print(mul(x,y))
